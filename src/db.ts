1357efa80a913c757802cbcbdc1d704391c7232b
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-02-24 21:16:26 -0300

    import models on db.ts

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,12 @@
 import { DataSource } from 'typeorm'
-import { Product } from './models/product'
+import { Cart } from './models/Cart'
+import { Favorite } from './models/Favorite'
+import { Product } from './models/Product'
+import { Review } from './models/Review'
+import { Subscribe } from './models/Subscribe'
 import { User } from './models/User'
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME } =
-  process.env
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME } = process.env
 
 const NUM_DB_PORT = Number(DB_PORT) // TypeORM expects a number
 
@@ -14,7 +17,7 @@ const db = new DataSource({
   password: DB_PASSWORD,
   port: NUM_DB_PORT,
   database: DB_NAME,
-  entities: [User, Product],
+  entities: [User, Product, Subscribe, Cart, Favorite, Review],
   logging: true,
   synchronize: true
 })
