184bb7ae0a7f15e5863afc38f5978f4ded32c711
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-02-24 21:14:52 -0300

    create favorite db model

---
+++ src/models/Favorite.ts
@@ -0,0 +1,26 @@
+import {
+  BaseEntity,
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  OneToOne,
+  ManyToMany,
+  JoinColumn,
+  JoinTable
+} from 'typeorm'
+import { Product } from './Product'
+import { User } from './User'
+
+@Entity()
+export class Favorite extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @OneToOne(() => User)
+  @JoinColumn()
+  user: User
+
+  @ManyToMany(() => Product)
+  @JoinTable()
+  products: Product
+}
