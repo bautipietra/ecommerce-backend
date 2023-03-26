44b2f6a77cb9324b1504d412ba68ecd9b74ebe16
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-25 21:36:09 -0300

    update imports in db

--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,4 @@
 import { DataSource } from 'typeorm'
-import { Cart } from './models/Cart'
-import { Favorite } from './models/Favorite'
-import { Product } from './models/Product'
-import { Review } from './models/Review'
-import { Subscribe } from './models/Subscribe'
 import { User } from './models/User'
 
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_NAME } = process.env
@@ -17,7 +12,7 @@ const db = new DataSource({
   password: DB_PASSWORD,
   port: NUM_DB_PORT,
   database: DB_NAME,
-  entities: [User, Product, Subscribe, Cart, Favorite, Review],
+  entities: [User],
   logging: true,
   synchronize: true
 })
