5124250345bb147f878dea9ba17ff87ae2f94146
Bautista Pietraroia <bautistapietraroia@gmail.com>
2023-03-25 21:36:18 -0300

    create review controller

---
+++ src/contollers/review.controllers.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express'
+import { User } from '../models/User'
+
+export const createSubscribe = async (req: Request, res: Response) => {
+  try {
+    const { email, review, rating } = req.body
+
+    /* If email doesn't exist */
+    const user = await User.findOne({
+      where: { email: email }
+    })
+    if (!user)
+      return res
+        .status(400)
+        .send({ message: "The email isn't registered" })
+
+    /* If email already sends a review */
+    if (!user.review)
+      return res
+        .status(400)
+        .send({ message: 'You have already send a review' })
+
+    /* Send a review */
+    user.review = {
+      review,
+      rating
+    }
+    User.update(user.id, user)
+    res.send(user)
+  } catch (error) {
+    error instanceof Error &&
+      res.status(500).send({ message: error.message })
+  }
+}
